6a165a57fe6fada3e59e062427c7dc79096bd66c
onni82 <onni.bucht@me.com>
2024-11-04 00:33:58 +0100

    Added Contains(), Find() and FindAll()

--- Lab-7-OOP-Generic-collections/Program.cs
+++ Lab-7-OOP-Generic-collections/Program.cs
@@ -5,6 +5,7 @@
 		static void Main(string[] args)
 		{
 			// PART 1 - STACK
+			Console.WriteLine($"PART 1 - STACK\n");
 			// creates a new stack
 			Stack<Employee> stack = new Stack<Employee>();
 
@@ -58,6 +59,7 @@
 
 			// PART 2 - LIST
 			// creates a new list
+			Console.WriteLine($"PART 2 - LIST\n");
 			List<Employee> list = new List<Employee>();
 			list.Add(new Employee(1, "Andrew", "male", 42000));
 			list.Add(new Employee(2, "Brenda", "female", 45000));
@@ -65,7 +67,23 @@
 			list.Add(new Employee(4, "Diane", "female", 36000));
 			list.Add(new Employee(5, "Eric", "male", 38000));
 
+			// creates an if statement that uses Contains() to check if a specific employee exists in the list
+			if (list.Contains(new Employee(2, "Brenda", "female", 45000)))
+			{
+				Console.WriteLine("\nEmployee 2 exists in the list");
+			}
 
+			// use Find() to find the first male employee in the list
+			Employee firstMaleEmployee = list.Find(employee => employee.Gender == "male");
+			Console.WriteLine($"\nFirst male employee in the list: {firstMaleEmployee}");
+
+			// use FindAll() to find all male employees in the list
+			List<Employee> allMaleEmployees = list.FindAll(employee => employee.Gender == "male");
+			Console.WriteLine("\nAll male employees in the list:");
+			foreach (Employee employee in allMaleEmployees)
+			{
+				Console.WriteLine(employee);
+			}
 		}
 	}
 }
